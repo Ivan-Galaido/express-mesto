02a323253580939ad2f7d9c4e95185fe5f1389f8
Ivan-Galaido <75268958+Ivan-Galaido@users.noreply.github.com>
2021-08-18 20:40:49 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,18 +6,15 @@ const UnauthorizedError = require('../errors/401-unauthorized-error');
 dotenv.config();
 const { JWT_SECRET, NODE_ENV } = process.env;
 
-exports.Auth = (req, res, next) => { 
-
-const token = req.cookies.userToken;
-let payload;
-
-try {
+exports.Auth = (req, res, next) => {
+  const token = req.cookies.userToken;
+  let payload;
+  try {
   // верифицируем токен
-    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
-  } catch (err) {
+  payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
+} catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
-
   req.user = payload; // записываем пейлоуд в объект запроса
   next(); // пропускаем запрос дальше
 };
